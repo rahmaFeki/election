969e6f441293ac9114126e404c868c9aba56f38f
rahmaFeki <rahma19fki@gmail.com>
2021-11-02 22:09:22 +0100

    class admin

---
+++ source/repos/election/election/Administrateur.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Administrateur
+{
+    public int id_admin { get; set; }
+  
+    public string nom_admin { get; set; }
+    public string prenom_admin { get; set; }
+    public string cin_admin { get; set; }
+    public string mot_de_passe { get; set; }
+   
+}
