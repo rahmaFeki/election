2450ac17b1dc7899c9609aaf61c5868302682811
marwa-baklouti <marwa.baklouti66@gmail.com>
2021-11-02 22:17:19 +0100

    candidat class

---
+++ source/repos/election/election/Candidat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace election
+{
+    class Candidat
+    {
+        public int id_candidat { get; set; }
+        public string nom_candidat { get; set; }
+        public string prenom_candidat { get; set; }
+        public string cin_candidat { get; set; }
+        public string Image_candidat { get; set; }
+
+    }
+}
