d5990146c607165a29dd6659a1f0df5324ead7c5
arwayh01 <73495454+arwayh01@users.noreply.github.com>
2021-11-07 17:15:03 +0100

    correction classe admin

--- source/repos/election/election/Administrateur.cs
+++ source/repos/election/election/Administrateur.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class Administrateur
+namespace election
 {
-    public int id_admin { get; set; }
-  
-    public string nom_admin { get; set; }
-    public string prenom_admin { get; set; }
-    public string cin_admin { get; set; }
-    public string mot_de_passe { get; set; }
-    public IList<election.Candidat> Candidats { get; set; }
-   
-    public CentreElection CentreElection { get; set;}
+    public class Administrateur
+    {
+        public int id_admin { get; set; }
 
-   
+        public string nom_admin { get; set; }
+        public string prenom_admin { get; set; }
+        public string cin_admin { get; set; }
+        public string mot_de_passe { get; set; }
+        public IList<election.Candidat> Candidats { get; set; }
+
+        public CentreElection CentreElection { get; set; }
+
+
+    }
 }
